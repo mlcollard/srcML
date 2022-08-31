fd07532395e374c1c0e826ab929fe6a3c0bf38a5
Michael L. Collard <collard@uakron.edu>
2022-08-31 08:19:08 -0400

    Fix conversion warning on Windows

--- src/client/XCPUCount.cpp
+++ src/client/XCPUCount.cpp
@@ -43,7 +43,7 @@ int CPUCount() {
 #ifdef _WIN64
     SYSTEM_INFO sysinfo;
     GetSystemInfo(&sysinfo);
-    return sysinfo.dwNumberOfProcessors;
+    return (int) sysinfo.dwNumberOfProcessors;
 #elif __APPLE__
     int count = 0;
     size_t len = sizeof(count);
