9984042da85e5fb4bd17aa5a20c923901e010ab4
Michael L. Collard <collard@uakron.edu>
2022-09-19 13:51:41 -0400

    Update installer to what we have learned from Greetings

--- package/nsis.cmake
+++ package/nsis.cmake
@@ -17,11 +17,9 @@
 # along with the srcML Toolkit; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-# set icons
+# set icon
 install(FILES ${CMAKE_SOURCE_DIR}/package/srcml_icon.ico DESTINATION . COMPONENT SRCML)
-set(CPACK_NSIS_MUI_ICON ${CMAKE_SOURCE_DIR}/package/srcml_icon.ico)
-set(CPACK_NSIS_MUI_UNIICON ${CMAKE_SOURCE_DIR}/package/srcml_icon.ico)
-set(CPACK_NSIS_INSTALLED_ICON_NAME srcml_icon.ico)
+set(CPACK_WIX_PRODUCT_ICON ${CMAKE_SOURCE_DIR}/package/srcml_icon.ico)
 
 # already set
 set(CPACK_NSIS_MODIFY_PATH ON)
@@ -32,14 +30,26 @@ set(CPACK_PACKAGE_INSTALL_DIRECTORY "srcML")
 # set contact in add/remove programs
 set(CPACK_NSIS_CONTACT "srcML <srcML.org>")
 
-# which components are disabled
-# set(CPACK_COMPONENT_SRCML_DISABLED False)
-# set(CPACK_COMPONENT_DEVLIBS_DISABLED False)
-# set(CPACK_COMPONENT_SRCMLDEV_DISABLED True)
-# set(CPACK_COMPONENT_EXAMPLES_DISABLED True)
+# Custom GUID
+set(CPACK_WIX_PRODUCT_GUID "8EB65BD8-DF5B-4DED-A642-80BDBBDEB58C")
 
-set(CPACK_WIX_EXTRA_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/srcml_extra_dialog.wxs")
+# Custom GUID
+set(CPACK_WIX_UPGRADE_GUID "278604DF-BD50-410A-9475-993006D98663")
 
-# set(CPACK_WIX_PATCH_FILE "${CMAKE_CURRENT_SOURCE_DIR}/patch_path_env.xml")
+# Background on dialogs
+set(CPACK_WIX_UI_DIALOG "${CMAKE_CURRENT_SOURCE_DIR}/background.png")
 
+# ID of custom UI
+set(CPACK_WIX_UI_REF "srcMLUI_InstallDir")
+
+# Add to CMake registry so that other CMake files can use find_program()
+set(CPACK_WIX_CMAKE_PACKAGE_REGISTRY srcML)
+
+# Extra dialog for adding to path
+set(CPACK_WIX_EXTRA_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/srcml_extra_dialog.wxs" "${CMAKE_CURRENT_SOURCE_DIR}/install_dir.wxs")
+
+# Path for path environment
+set(CPACK_WIX_PATCH_FILE "${CMAKE_CURRENT_SOURCE_DIR}/patch_path_env.xml")
+
+# Template instead of default WiX template
 set(CPACK_WIX_TEMPLATE "${CMAKE_CURRENT_SOURCE_DIR}/WIX.template.in")
