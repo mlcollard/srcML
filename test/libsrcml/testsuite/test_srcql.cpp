17f0f061cf61a58bcb098d38c2c9a22f097b4abc
Behler <jbehler1@kent.edu>
2025-03-07 09:33:54 -0500

    Add an additional test for correct clearing.

--- test/libsrcml/testsuite/test_srcql.cpp
+++ test/libsrcml/testsuite/test_srcql.cpp
@@ -5940,6 +5940,58 @@ int bar(char addr) { return id; }
         srcml_archive_free(iarchive);
     }
 
+    const std::string correct_clear_with_multiple_clears_at_different_levels = R"(
+class X {
+    A1 foo(B1 p1) { return p2; }
+};
+
+class Y {
+    A2 foo(B2 p3) { return p4; }
+};
+
+class Z {
+    X foo(Y p5) { return p5; }
+    Z bar(Y p6) { return p6; }
+    Y bax(Z p7) { return p7; }
+    Z baz(Z p8) { return p7; }
+};
+)";
+
+    // FIND class $CLS {}; CONTAINS return $PARAM; WITHIN $CLS $FNAME($CLS $PARAM) {}
+    {
+        char* s;
+        size_t size;
+
+        srcml_archive* oarchive = srcml_archive_create();
+        srcml_archive_write_open_memory(oarchive,&s, &size);
+
+        srcml_unit* unit = srcml_unit_create(oarchive);
+        srcml_unit_set_language(unit,"C++");
+        srcml_unit_parse_memory(unit,correct_clear_with_multiple_clears_at_different_levels.c_str(),correct_clear_with_multiple_clears_at_different_levels.size());
+        dassert(srcml_archive_write_unit(oarchive,unit), SRCML_STATUS_OK);
+
+        srcml_unit_free(unit);
+        srcml_archive_close(oarchive);
+        srcml_archive_free(oarchive);
+
+        std::string srcml_text = std::string(s, size);
+        free(s);
+
+        srcml_archive* iarchive = srcml_archive_create();
+        srcml_archive_read_open_memory(iarchive,srcml_text.c_str(),srcml_text.size());
+        dassert(srcml_append_transform_srcql(iarchive,"FIND class $CLS {}; CONTAINS return $PARAM; WITHIN $CLS $FNAME($CLS $PARAM) {}"), SRCML_STATUS_OK);
+
+        unit = srcml_archive_read_unit(iarchive);
+        srcml_transform_result* result = nullptr;
+        srcml_unit_apply_transforms(iarchive, unit, &result);
+
+        dassert(srcml_transform_get_type(result), SRCML_RESULT_NONE);
+
+        srcml_unit_free(unit);
+        srcml_transform_free(result);
+        srcml_archive_close(iarchive);
+        srcml_archive_free(iarchive);
+    }
 
 
     const std::string various_contains_exprs_src = R"(
