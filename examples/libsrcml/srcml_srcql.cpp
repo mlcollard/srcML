4b61413df0bab849a7e2ca15ee31a7c40d19bd97
Behler <jbehler1@kent.edu>
2025-07-08 14:33:17 -0400

    Add libsrcml srcQL example

---
+++ examples/libsrcml/srcml_srcql.cpp
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/**
+ * @file srcml_xpath.cpp
+ *
+ * @copyright Copyright (C) 2013-2024 srcML, LLC. (www.srcML.org)
+ *
+ * Example program of the use of the libsrcml C API.
+ *
+ * Applies a srcQL query. The query searches for all decls and exprs
+ *
+ * Equivalent to the srcml command line:
+ *
+ * srcml -j 1 --srcql='FIND $X' project.xml -o srcql.xml
+*/
+
+#include <srcml.h>
+
+int main(int argc, char * argv[]) {
+
+    // open the input archive
+    srcml_archive* inputArchive = srcml_archive_create();
+    srcml_archive_read_open_filename(inputArchive, "project.xml");
+
+    // add the xpath transformation
+    srcml_append_transform_srcql(inputArchive, "FIND $X");
+
+    // open the output archive
+    srcml_archive* outputArchive = srcml_archive_clone(inputArchive);
+    srcml_archive_write_open_filename(outputArchive, "srcql.xml");
+
+    // apply the transformation to the archive
+    srcml_unit* unit = nullptr;
+    while ((unit = srcml_archive_read_unit(inputArchive))) {
+
+        // apply the transforms
+        // since an XPath transformation can produce multiple units
+        // from a single input unit, this requires using the srcml_transform_result
+        srcml_transform_result* result = nullptr;
+        srcml_unit_apply_transforms(inputArchive, unit, &result);
+
+        // write the transform result
+        for (int i = 0; i < srcml_transform_get_unit_size(result); ++i) {
+            srcml_archive_write_unit(outputArchive, srcml_transform_get_unit(result, i));
+        }
+
+        // free the transformation result
+        srcml_transform_free(result);
+
+        // free the input unit
+        srcml_unit_free(unit);
+    }
+
+    // close the archives
+    srcml_archive_close(inputArchive);
+    srcml_archive_close(outputArchive);
+
+    // free the archives
+    srcml_archive_free(inputArchive);
+    srcml_archive_free(outputArchive);
+
+    return 0;
+}
